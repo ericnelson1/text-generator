d51085253e6be0de6cb51fca43b5228763bba012
ericnelson1 <ericnelson6@gmail.com>
2015-11-25 02:44:21 -0600

    working on api

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');  // or use express body parser instead?
 var textEngine = require('./text-engine');
+var url = require('url');
 
 var app = module.exports.app = exports.app = express();
 app.use(bodyParser.json()); // support json encoded bodies
@@ -13,19 +14,34 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 //app.use(express.static('public'));
 
 app.get('/api', function(req, res) {
-	res.send(textEngine.getEntries());
+	var hostname = req.headers.host;
+	var root = {
+		links: 'http://' + hostname + '/api/links',
+		stats: 'http://' + hostname + '/api/stats'
+	}
+	res.send(root);
+});
+
+app.get('/api/links', function(req, res) {
+	textEngine.getLinks().then(function(res) {
+		res.send(res);
+	});
 });
  
+app.post('/api/link', function(req, res) {
+	textEngine.process(req.body.url);
+	res.send({a:'hello', b:'bye', c: req.body.id});
+});
+ 
+app.get('/api/misc', function(req, res) {
+	res.send(textEngine.getEntries());
+});
+
 app.get('/api/text/:id', function(req, res) {
 	var entry = textEngine.getEntry(req.params.id);
     res.send(entry);
 });
 
-app.post('/api/text', function(req, res) {
-	textEngine.process(req.body.url);
-	res.send({a:'hello', b:'bye', c: req.body.id});
-});
- 
 app.listen(3000);
 console.log("listening on port 3000");
 
--- api/text-engine.js
+++ api/text-engine.js
@@ -1,10 +1,16 @@
-var redis = require("redis"),
+var redis = require('redis'),
+    bluebird = require('bluebird'),
     client = redis.createClient();
+
+// promisify redis
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
 
 // if you'd like to select database 3, instead of 0 (default), call
 // client.select(3, function() { /* ... */ });
 
-client.on("error", function (err) {
+client.on('error', function (err) {
     console.log("Error " + err);
 });
 
@@ -19,24 +25,14 @@ client.hkeys("hash key", function (err, replies) {
     client.quit();
 });
 
-var entries = [
-{"id":1, "title":"Hello World!", "body":"This is the body of my blog entry. Sooo exciting.", "published":"6/2/2013"},
-{"id":2, "title":"Eggs for Breakfast", "body":"Today I had eggs for breakfast. Sooo exciting.", "published":"6/3/2013"},
-{"id":3, "title":"Beer is Good", "body":"News Flash! Beer is awesome!", "published":"6/4/2013"},
-{"id":4, "title":"Mean People Suck", "body":"People who are mean aren't nice or fun to hang around.", "published":"6/5/2013"},
-{"id":5, "title":"I'm Leaving Technology X and You Care", "body":"Let me write some link bait about why I'm not using a particular technology anymore.", "published":"6/10/2013"},
-{"id":6, "title":"Help My Kickstarter", "body":"I want a new XBox One. Please fund my Kickstarter.", "published":"6/12/2013"}];
+client.rpush('mylinks', 'hello');
+client.rpush('mylinks', 'world');
  
-exports.setEntry = function(entry) {
-	client.set("")
+exports.submitLink = function(entry) {
+	client.set("");
 };
  
-exports.getEntries = function() {
-    return entries;
+exports.getLinks = function() {
+    return client.lrangeAsync('mylinks', 0, -1);
 };
  
-exports.getEntry = function(id) {
-    for(var i=0; i < entries.length; i++) {
-        if(entries[i].id == id) return entries[i];
-    }
-};
