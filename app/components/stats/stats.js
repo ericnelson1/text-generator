9616485e4217ed30572fa2835c2598a69cfa7e3c
ericnelson1 <ericnelson6@gmail.com>
2015-11-28 22:58:37 -0600

    stats ui

--- app/components/stats/stats.js
+++ app/components/stats/stats.js
@@ -4,7 +4,40 @@
 angular.module('app.controllers')
 .controller('StatsController', 
   function() {
+
+	this.depths = [
+		{display: 'One Character', depth: 1},
+		{display: 'Four Characters', depth: 4},
+		{display: 'Eight Characters', depth: 8}
+	];
+
+	this.selectedDepth = this.depths[1];
+
+	this.selectDepth = function(d) {
+		this.selectedDepth = d;
+	};
+
+	this.charts = [
+		{display: 'Bar Graph' },
+		{display: 'Bubble Chart' }
+	];
+
+	this.selectedChart = this.charts[0];
+
+	this.selectChart = function(c) {
+		this.selectedChart = c;
+	};
  
+	this.sorts = [
+		{display: 'Alphabetical' },
+		{display: 'Count' }
+	];
+
+	this.selectedSort = this.sorts[1];
+
+	this.selectSort = function(s) {
+		this.selectedSort = s;
+	};
 });
 
 })();
